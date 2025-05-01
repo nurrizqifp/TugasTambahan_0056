c38c29ca5c67a713cecefc2b318ac2037048df79
nurrizqifp <nurrizqifebriansyah@gmail.com>
2025-05-01 08:49:45 +0700

    Membuat tampilan untuk input data (alas dan tinggi)

--- tugas2.cpp
+++ tugas2.cpp
@@ -8,6 +8,10 @@ class Segitiga{
         bool hitung;
 
         void inputData(){
+            cout << "Masukkan alas : ";
+            cin >> alas;
+            cout << "Masukkan tinggi : ";
+            cin >> tinggi;
         }
     private :
     public  :
