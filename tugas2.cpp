cfab3aad5d813a699c839da128cc23b141ed2909
nurrizqifp <nurrizqifebriansyah@gmail.com>
2025-05-01 09:06:39 +0700

    Menambahkan validasi bilangan ganjil atau selain ganjil untuk variabel alas

--- tugas2.cpp
+++ tugas2.cpp
@@ -12,6 +12,13 @@ class Segitiga{
             cin >> alas;
             cout << "Masukkan tinggi : ";
             cin >> tinggi;
+
+            if (alas % 2 == 1){ // validasi bilangan bulat ganjil
+                hitung = false; // alas dengan bilangan ganjil tidak dihitung
+            }
+            else {
+                hitung = true;  // alas dengan bilangan genap akan dihitung
+            }
         }
     private :
     public  :
